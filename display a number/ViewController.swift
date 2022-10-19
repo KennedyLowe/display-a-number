8c1976459d1186cd8a56eb73d69ba667fe462013
Kennedy Lowe <klowe040@d127.org>
2022-10-19 09:30:04 -0500

    Initial Commit

---
+++ display a number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  display a number
+//
+//  Created by Kennedy P Lowe (Student) on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
